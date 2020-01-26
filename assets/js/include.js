ef9b958b4ba40ecb416a9125d158e1adf02b85aa
sjsh1623 <sjsh1623@naver.com>
2020-01-26 13:13:26 +0900

    Setting up

---
+++ assets/js/include.js
@@ -0,0 +1,18 @@
+/*
+* This is a function to call HEADER html
+* This function also require JQuery
+* depth level : 1
+*/
+$(document).ready(function(){
+    $("#header").load("inc/header.html")
+});
+
+
+/*
+* This is a function to call FOOTER html
+* This function also require JQuery
+* depth level : 1
+*/
+$(document).ready(function(){
+    $("#footer").load("inc/footer.html")
+});
