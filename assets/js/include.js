80e2345ac1f8f8346a50a7859e4daec864419ad7
sjsh1623 <sjsh1623@naver.com>
2020-01-27 14:27:15 +0900

    update

--- assets/js/include.js
+++ assets/js/include.js
@@ -18,3 +18,23 @@ $(document).ready(function () {
     $("#frontEnd").load("inc/category/FrontEnd.html")
 });
 
+/*
+* This is a function to call BackEnd html
+* This function also require JQuery
+* depth level : 1
+*/
+
+
+$(document).ready(function () {
+    $("#backEnd").load("inc/category/BackEnd.html")
+});
+
+/*
+* This is a function to call DataBase html
+* This function also require JQuery
+* depth level : 1
+*/
+
+$(document).ready(function () {
+    $("#database").load("inc/category/DataBase.html")
+});
